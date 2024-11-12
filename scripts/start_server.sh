5f6923224b132a7735fc079432c333b41cea1c69
Jake McGuire <120243972+jmcguire001@users.noreply.github.com>
2024-11-12 11:23:03 -0700

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/paradisedonutcompany
+cd /home/ec2-user/findmyfood
 dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -o /home/ec2-user/prod
-sudo systemctl start pdc.service
+sudo systemctl start findmyfood.service
 sudo systemctl start nginx
