0a51e1e33c270058932dfc473e243feb0dc00b63
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-11-12 10:58:39 -0700

    Add project files.

---
+++ findmyfood/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace findmyfood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
